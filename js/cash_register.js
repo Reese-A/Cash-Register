baa938d25a76a16c386b1249e2eadc2b678262b4
Reese Akamine <r.akamine@Reeses-MacBook-Pro.local>
2018-04-03 10:52:25 -1000

    add and equals working

--- js/cash_register.js
+++ js/cash_register.js
@@ -1,29 +1,45 @@
 
 let calc = calculatorModule();
 let valueStr = "";
-let valueNum = Number.parseFloat(display.innerHTML);
+let displayNum = 0;
+let valueNum = 0;
+let addCheck = false;
 
 
 let numButtons = document.getElementsByClassName('numButton');
 for(i=0;i<numButtons.length;i++){
     numButtons[i].addEventListener('click', storeValue)
 }
-
+ 
 
 function storeValue(){
-    valueStr += this.innerHTML;
+    valueStr += this.innerHTML; 
     display.innerHTML = valueStr;
+    valueNum = Number.parseFloat(display.innerHTML);
+    displayNum = Number.parseFloat(display.innerHTML);
 };
-
-equals.addEventListener('click', totalGet)
-function totalGet(){
-    display.innerHTML = calc.getTotal();
-    console.log(calc.getTotal());
-}
 
 addButton.addEventListener('click', addFunction);
 function addFunction(){
-    calc.load(valueNum);
     valueStr = "";
-    display.innerHTML = 0;
+    display.innerHTML = '+';
+    calc.add(displayNum);
+    valueNum = calc.getTotal();
+    calc.load(valueNum);
+    console.log(valueNum);
+    if(addCheck === false){
+        addCheck = true;
+    }else{
+        addCheck = false;
+    }
+    }
+
+equals.addEventListener('click', totalGet)
+function totalGet() {
+    if(addCheck === true){
+        calc.add(displayNum);
+        addCheck = false;
+    }
+    display.innerHTML = calc.getTotal();
+    console.log(calc.getTotal());
 }
